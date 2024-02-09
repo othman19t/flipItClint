61f715bfe3f081e0366a0e49aef514360b18066e
Basil Kawak <basilalisa0@gmail.com>
2024-02-09 23:04:52 +0300

    create card component

---
+++ src/components/card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = ({ post }) => {
+  const handleCardClick = () => {
+    window.open(post.postUrl, "_blank");
+  };
+
+  return (
+    <div className="card" onClick={handleCardClick}>
+      <img src={post.imgSrc[0]} alt={post.title} />
+      <div className="card-info">
+        <h4>{post.title}</h4>
+        <p>{post.price}</p>
+        <p>{post.location}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
