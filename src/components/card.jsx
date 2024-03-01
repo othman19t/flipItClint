ee81d59c512150d3deb476d82001b0fedeb3d780
Othman Othman <othman19t@gmail.com>
2024-03-01 06:10:21 -0800

    maintence

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -10,9 +10,23 @@ const Card = ({ post }) => {
     <div className='card' onClick={handleCardClick}>
       <img src={img} alt={post?.title} />
       <div className='card-info'>
-        <h4>{post?.title}</h4>
-        <p>{post?.price}</p>
-        <p>{post?.location}</p>
+        <div className='tags_and_location'>
+          <p>{post?.location}</p>
+          <div className='tag_con'>
+            <p className='card_distance'>{post?.distance}km</p>
+            <p className='card_price'>{post?.price}</p>
+          </div>
+        </div>
+        <p className='card_title'>
+          {post?.title?.length > 25
+            ? post?.title?.slice(0, 25) + '...'
+            : post?.title}
+        </p>
+        <p className='card_description'>
+          {post?.description?.length > 100
+            ? post?.description?.slice(0, 100) + '...'
+            : post?.description}
+        </p>
       </div>
     </div>
   );
